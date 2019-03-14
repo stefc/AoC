0068c938b95d241544d827eed9cdb0cebebf6258
Stefc <stefan.boether@gmail.com>
2019-03-14 10:32:50 +0100

    Erstes Rätsel von 2015 als Rahmen inkl. Tests definiert

---
+++ tests/y2015/Day1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+using advent.of.code.y2015.day1;
+
+
+namespace advent.of.code.tests.y2015 {
+
+    public class TestDay1
+    {
+        [Theory]
+        [InlineData("(())",0)]
+        [InlineData("()()",0)]
+        [InlineData("(((",3)]
+        [InlineData("(()(()(",3)]
+        [InlineData("))(((((",3)]
+        [InlineData("())",-1)]
+        [InlineData("))(",-1)]
+        [InlineData(")())())",-3)]
+        [InlineData(")))",-3)]
+        public void PartOne(string instructions, int expectedFloor)
+        {
+            Assert.Equal(expectedFloor, 
+                NotQuiteLisp.WhatFloor(instructions));
+        }
+        
+    
+    }
+}
---
+++ y2015/day1/NotQuiteLisp.cs
@@ -0,0 +1,15 @@
+// http://adventofcode.com/2015/day/1
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace advent.of.code.y2015.day1 {
+
+    static class NotQuiteLisp {
+        
+        public static int WhatFloor(string instructions) {
+            throw new NotImplementedException();
+        }
+    }
+}
