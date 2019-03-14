89f846b08df4b5fde068356813665bce8b0ed71c
Stefc <stefan.boether@gmail.com>
2019-03-14 10:47:07 +0100

    Rätselrahmen Tag1 aus 2016 aufgenommen

---
+++ y2016/day1/NoTimeForTaxicab.cs
@@ -0,0 +1,15 @@
+// http://adventofcode.com/2015/day/1
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace advent.of.code.y2016.day1 {
+
+    static class NoTimeForTaxicap {
+        
+        public static int HowFarBlocksAway(string instructions) {
+            throw new NotImplementedException();
+        }
+    }
+}
