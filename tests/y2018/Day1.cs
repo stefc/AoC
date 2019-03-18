0482fdb43856cbecb8a441637018484297953f5a
Stefc <stefan.boether@gmail.com>
2019-03-18 17:58:51 +0100

    Tag 1 in 2018 gelöst

--- tests/y2018/Day1.cs
+++ tests/y2018/Day1.cs
@@ -9,9 +9,10 @@ namespace advent.of.code.tests.y2018 {
     public class TestDay1
     {
         [Theory]
-        [InlineData(3,"+1, +1, +1")]
-        [InlineData(0,"+1, +1, -2")]
-        [InlineData(-6,"-1, -2, -3")]
+        [InlineData(3,"+1,-2,+3,+1")]
+        [InlineData(3,"+1,+1,+1")]
+        [InlineData(0,"+1,+1,-2")]
+        [InlineData(-6,"-1,-2,-3")]
         
         public void PartOne(int expected, string value)
         {
