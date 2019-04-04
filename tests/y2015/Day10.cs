c8f660fa859f0492626416dfc32f21587637bc03
Stefc <stefan.boether@gmail.com>
2019-04-04 05:34:55 +0200

    Part 2 von Tag 10

--- tests/y2015/Day10.cs
+++ tests/y2015/Day10.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 
 using advent.of.code.y2015.day10;
+using System.Numerics;
 /*
 example:
 
@@ -14,18 +15,48 @@ example:
 Starting with the digits in your puzzle input, apply this process 40 times. What is the length of the result?
 
 Your puzzle input is 1113222113.
- */
+*/
 
 namespace advent.of.code.tests.y2015 {
 
     public class TestDay10
     {
         [Theory]
-        [InlineData(111221,"312211")]
-        public void PartOne(ulong number, string expected)
+        [InlineData(1,1,"11")]
+        [InlineData(11,1,"21")]
+        [InlineData(21,1,"1211")]
+        [InlineData(1211,1,"111221")]
+        [InlineData(111221,1,"312211")]
+        [InlineData(1,3,"1211")]
+        [InlineData(1,5,"312211")]
+        public void PartOne(long number, int iteration, string expected)
         {
             Assert.Equal(expected,
-                LookAndSay.Iterate(number, iteration, 1));
+                LookAndSay.Iterate(number, iteration));
+        }
+
+        [Fact]
+        public void PuzzleA()
+        {
+
+            //Assert.Equal(252594,
+            //    LookAndSay.Iterate(1113222113, 40).Length);
+        }
+
+        [Fact]
+        public void Puzzle()
+        {
+            var result = LookAndSay.Polynomial(4);
+
+
+            //var partA = LookAndSay.Iterate(new BigInteger(1113222113), 40);
+
+            //Assert.Equal(252594, partA.ToString().Length);
+            
+            //var partB = LookAndSay.Iterate(partA, 50);
+
+            //Assert.Equal(252594, partB.ToString().Length);
+
         }
     }
 }
