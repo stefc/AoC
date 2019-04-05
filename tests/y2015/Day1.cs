0d465c2ba1cd0cd8ed6432a0557eb4a3a2e38b3f
Stefc <stefan.boether@gmail.com>
2019-04-05 07:23:57 +0200

    Tag 1 2015 vorbereitet

--- tests/y2015/Day1.cs
+++ tests/y2015/Day1.cs
@@ -2,13 +2,13 @@ using System;
 using Xunit;
 
 using advent.of.code.y2015.day1;
-
+using System.IO;
 
 namespace advent.of.code.tests.y2015 {
 
     public class TestDay1
     {
-/*         [Theory]
+        [Theory]
         [InlineData("(())",0)]
         [InlineData("()()",0)]
         [InlineData("(((",3)]
@@ -18,12 +18,24 @@ namespace advent.of.code.tests.y2015 {
         [InlineData("))(",-1)]
         [InlineData(")())())",-3)]
         [InlineData(")))",-3)]
-        public void PartOne(string instructions, int expectedFloor)
-        {
-            Assert.Equal(expectedFloor, 
-                NotQuiteLisp.WhatFloor(instructions));
+        public void PartOne(string instructions, int expectedFloor) 
+            =>
+                Assert.Equal(expectedFloor, 
+                    NotQuiteLisp.WhatFloor(instructions));
+
+        [Theory]
+        [InlineData("()())",5)]
+        public void PartTwo(string instructions, int expected) 
+            =>
+                Assert.Equal(expected,
+                    NotQuiteLisp.HowManyMovesToBasement(instructions));
+
+        [Fact]
+        public void Puzzle() {
+            string instructions = File.ReadAllText("day1.input.txt");
+            
+            Assert.Equal(2323, NotQuiteLisp.WhatFloor(instructions));
+            Assert.Equal(2323, NotQuiteLisp.HowManyMovesToBasement(instructions));
         }
-*/        
-    
     }
 }
--- y2015/day1/NotQuiteLisp.cs
+++ y2015/day1/NotQuiteLisp.cs
@@ -8,8 +8,20 @@ namespace advent.of.code.y2015.day1 {
 
     static class NotQuiteLisp {
         
-        public static int WhatFloor(string instructions) {
-            throw new NotImplementedException();
-        }
+        public static int WhatFloor(string instructions) =>
+            instructions
+                .Select(MovementSign)
+                .Sum();
+
+        public static int HowManyMovesToBasement(string instructions) =>
+            instructions
+                .Select(MovementSign)
+                .Aggregate(
+                    seed:(count:0, floor:0),
+                    func: (accu,current) 
+                        => accu.floor < 0 ? accu : (count:accu.count+1, floor: accu.floor+current),
+                    resultSelector: accu => accu.count);
+                    
+        private static int MovementSign(char movement) => movement == '(' ? +1 : -1;
     }
 }
