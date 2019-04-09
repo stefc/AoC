baedf51f390300613f6a6976e073ca2ea64a5f34
Stefc <stefan.boether@gmail.com>
2019-04-09 17:59:01 +0200

    Day2 finished

---
+++ tests/y2015/Day2.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+
+using advent.of.code.y2015.day2;
+using System.IO;
+using System.Linq;
+
+namespace advent.of.code.tests.y2015 {
+
+    public class TestDay2
+    {
+        [Theory]
+        [InlineData("2x3x4",58)]
+        [InlineData("1x1x10",43)]
+        public void PartOne(string dimensions, int expected) 
+            =>
+                Assert.Equal(expected, 
+                    WrappingPaper.SquareFeetOfPaper(dimensions));
+
+        [Theory]
+        [InlineData("2x3x4",34)]
+        [InlineData("1x1x10",14)]
+        public void PartTwo(string dimensions, int expected) 
+            =>
+                Assert.Equal(expected, 
+                    WrappingPaper.FeetOfRibbon(dimensions));
+
+        [Fact]
+        public void Puzzle() {
+            var input = File
+                .ReadLines("tests/y2015/Day2.Input.txt");
+
+            var paper = input
+                .Select(WrappingPaper.SquareFeetOfPaper)
+                .Sum();
+
+            Assert.Equal(1586300, paper);
+
+            var ribbon = input
+                .Select(WrappingPaper.FeetOfRibbon)
+                .Sum();
+
+            Assert.Equal(3737498, ribbon);
+        }
+    }
+}
