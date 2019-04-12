7a76e34d57f51400b84c873bfb79a0223f8d2903
Stefc <stefan.boether@gmail.com>
2019-04-12 17:41:42 +0200

    Tag3 abgeschlossen

--- tests/y2015/Day3.cs
+++ tests/y2015/Day3.cs
@@ -1,9 +1,10 @@
-using System;
-using Xunit;
+// http://adventofcode.com/2015/day/3
 
-using advent.of.code.y2015.day3;
+using System;
 using System.IO;
 using System.Linq;
+using Xunit;
+using advent.of.code.y2015.day3;
 
 namespace advent.of.code.tests.y2015 {
 
@@ -17,9 +18,9 @@ namespace advent.of.code.tests.y2015 {
             =>
                 Assert.Equal(expected, 
                     SphericalHouses.AtLeastOnePresent(instructions));
-        
+
         [Theory]
-        [InlineData(">",3)]
+        [InlineData("^v",3)]
         [InlineData("^>v<",3)]
         [InlineData("^v^v^v^v^v",11)]
         public void PartTwo(string instructions, int expected) 
@@ -34,27 +35,9 @@ namespace advent.of.code.tests.y2015 {
 
             Assert.Equal(2081,
                 SphericalHouses.AtLeastOnePresent(input));
-
-
-         /*    var paper = input
-                .Select(WrappingPaper.SquareFeetOfPaper)
-                .Sum();
-
-            Assert.Equal(1586300, paper); */
+            
+            Assert.Equal(2341,
+                SphericalHouses.TogetherWithRobodog(input));
         }
     }
 }
-
-/*
-The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
-
-Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
-
-This year, how many houses receive at least one present?
-
-For example:
-
-^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
-^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
-^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
- */
