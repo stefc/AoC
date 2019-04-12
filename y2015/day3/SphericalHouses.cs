e1bd67b81173afd67f6230d1a94eecd313c61e6e
Stefc <stefan.boether@gmail.com>
2019-04-12 17:41:42 +0200

    Tag3 abgeschlossen

--- y2015/day3/SphericalHouses.cs
+++ y2015/day3/SphericalHouses.cs
@@ -19,8 +19,20 @@ namespace advent.of.code.y2015.day3 {
 
     static class SphericalHouses {
         
-        public static int AtLeastOnePresent(string instructions)  
-        {
+        public static int AtLeastOnePresent(string instructions)  => GetVisits(instructions).Count;
+
+        public static int TogetherWithRobodog(string instructions) {
+            var result = instructions
+                .Select( (instruction, index) => new {instruction, isSanta = index % 2 == 0})
+                .GroupBy( x => x.isSanta, x => x.instruction)
+                .Aggregate( 
+                    seed: ImmutableHashSet<Point>.Empty,
+                    func: (accu,current) => accu.Union(GetVisits(string.Concat(current))),
+                    resultSelector: accu => accu.Count);
+            return result;
+        } 
+
+        private static ISet<Point> GetVisits(string instructions) {
             var initialResult = ImmutableHashSet<Point>.Empty.Add(Point.Zero);
             return instructions
                 .Select(InstructionToPoint)
@@ -31,14 +43,9 @@ namespace advent.of.code.y2015.day3 {
                         var newResult = accu.result.Add(newLocation);
                         return (location: newLocation, result: newResult);
                     },
-                    resultSelector: accu => accu.result.Count
-                ); 
-        }
-        public static int TogetherWithRobodog(string instructions) {
-            return 0;
+                    resultSelector: accu => accu.result
+                );
         } 
-
-
         private static Point InstructionToPoint(char instruction) {
             if (instruction == '<')
                 return Point.West;
