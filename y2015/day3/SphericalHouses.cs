17293e0bde487772ce0c9b6f4cf9446b79df63ea
Stefc <stefan.boether@gmail.com>
2019-04-27 17:49:00 +0200

    Tag #5 weiter

--- y2015/day3/SphericalHouses.cs
+++ y2015/day3/SphericalHouses.cs
@@ -1,14 +1,5 @@
-/* Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
 
-For example:
-
-A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
-A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
-All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
-
-*/
-
-// http://adventofcode.com/2015/day/2
+// http://adventofcode.com/2015/day/3
 
 using System;
 using System.Linq;
---
+++ y2015/day5/StringClassifier.cs
@@ -0,0 +1,93 @@
+
+// http://adventofcode.com/2015/day/5
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace advent.of.code.y2015.day5 {
+
+    static class StringClassifier {
+        
+        public static bool IsNice(string value)  => 
+            value.CountVowels() >= 3 && 
+            value.HasDuplicates() &&
+            !value.HasSpecialStrings();
+        
+        public static bool IsNicer(string value)  => 
+            value.HasPair() && 
+            value.HasSurounding();
+
+        private static int CountVowels(this string value) => value
+            .Where(ch => ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i')
+            .Count();
+
+        private static bool HasDuplicates(this string value) => value
+            .Aggregate(
+                seed: (result: false, state: new Nullable<char>()),
+                func: (accu,current) => (!accu.state.HasValue) 
+                    ?
+                    (result: accu.result, state: current) 
+                    :
+                    (result: accu.result || current.Equals(accu.state.Value), state: current),
+                resultSelector: accu => accu.result );
+
+        private static bool HasSpecialStrings(this string value) => 
+            value.Contains("ab") || value.Contains("cd") || 
+            value.Contains("pq") || value.Contains("xy");
+
+        public static bool HasPair(this string value) => value
+            .Aggregate(
+            
+                seed: (
+                    hashset: ImmutableHashSet<string>.Empty,
+                    result: false, 
+                    state: new Nullable<char>()),
+            
+                func: (accu,current) => { 
+                    if (!accu.state.HasValue) {
+                        return (hashset: accu.hashset, result: accu.result, state: current);
+                    } else {
+                        var tuple = accu.state.Value.ToString() + current;
+                        if (accu.hashset.Contains(tuple))
+                            return (hashset: accu.hashset, result: true, state: current);
+                        return (hashset: accu.hashset.Add(tuple), result: accu.result, state: current);
+                    }
+                },
+            
+                resultSelector: accu => accu.result 
+            );
+
+        public static bool HasSurounding(this string value) => value
+            .Aggregate(
+                seed: (
+                    queue: ImmutableQueue<char>.Empty,
+                    result: false 
+                ), 
+
+                func: (accu,current) => {
+                    if (accu.result)
+                        return accu;
+                    else if (accu.queue.Count() <= 2 )
+                        return (queue: accu.queue.Enqueue(current), 
+                            result: CheckSurrounding(accu.queue));
+
+                    return (queue: accu.queue.Enqueue(current).Dequeue(), 
+                        result: CheckSurrounding(accu.queue));
+                }, 
+
+                resultSelector: accu => accu.result
+            );
+
+        private static bool CheckSurrounding(IEnumerable<char> queue) {
+
+            if (queue.Count() <=2)
+                return false;
+            var content = String.Concat(queue);
+            var hasSurounding = content.Last() == content.First();
+            return hasSurounding;
+                    
+        }
+    }
+}
