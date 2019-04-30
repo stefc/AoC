36c3ee31f4ae380177e6e096b01a174088ce5cd7
Stefc <stefan.boether@gmail.com>
2019-04-30 08:39:20 +0200

    day5 fertig ?

--- y2015/day5/StringClassifier.cs
+++ y2015/day5/StringClassifier.cs
@@ -8,86 +8,84 @@ using System.Collections.Immutable;
 
 namespace advent.of.code.y2015.day5 {
 
-    static class StringClassifier {
-        
-        public static bool IsNice(string value)  => 
-            value.CountVowels() >= 3 && 
-            value.HasDuplicates() &&
-            !value.HasSpecialStrings();
-        
-        public static bool IsNicer(string value)  => 
-            value.HasPair() && 
-            value.HasSurounding();
+	static class StringClassifier {
 
-        private static int CountVowels(this string value) => value
-            .Where(ch => ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i')
-            .Count();
+		public static bool IsNice(string value)  =>
+			value.CountVowels() >= 3 &&
+			value.HasDuplicates() &&
+			!value.HasSpecialStrings();
 
-        private static bool HasDuplicates(this string value) => value
-            .Aggregate(
-                seed: (result: false, state: new Nullable<char>()),
-                func: (accu,current) => (!accu.state.HasValue) 
-                    ?
-                    (result: accu.result, state: current) 
-                    :
-                    (result: accu.result || current.Equals(accu.state.Value), state: current),
-                resultSelector: accu => accu.result );
+		public static bool IsNicer(string value)  =>
+			value.HasPair() &&
+			value.HasSurounding();
 
-        private static bool HasSpecialStrings(this string value) => 
-            value.Contains("ab") || value.Contains("cd") || 
-            value.Contains("pq") || value.Contains("xy");
+		private static int CountVowels(this string value) => value
+			.Where(ch => ch == 'a' || ch == 'e' || ch == 'o' || ch == 'u' || ch == 'i')
+			.Count();
 
-        public static bool HasPair(this string value) => value
-            .Aggregate(
-            
-                seed: (
-                    hashset: ImmutableHashSet<string>.Empty,
-                    result: false, 
-                    state: new Nullable<char>()),
-            
-                func: (accu,current) => { 
-                    if (!accu.state.HasValue) {
-                        return (hashset: accu.hashset, result: accu.result, state: current);
-                    } else {
-                        var tuple = accu.state.Value.ToString() + current;
-                        if (accu.hashset.Contains(tuple))
-                            return (hashset: accu.hashset, result: true, state: current);
-                        return (hashset: accu.hashset.Add(tuple), result: accu.result, state: current);
-                    }
-                },
-            
-                resultSelector: accu => accu.result 
-            );
+		private static bool HasDuplicates(this string value) => value
+			.Aggregate(
+				seed: (result: false, state: new Nullable<char>()),
+				func: (accu,current) => (!accu.state.HasValue)
+					?
+					(result: accu.result, state: current)
+					:
+					(result: accu.result || current.Equals(accu.state.Value), state: current),
+				resultSelector: accu => accu.result );
 
-        public static bool HasSurounding(this string value) => value
-            .Aggregate(
-                seed: (
-                    queue: ImmutableQueue<char>.Empty,
-                    result: false 
-                ), 
+		private static bool HasSpecialStrings(this string value) =>
+			value.Contains("ab") || value.Contains("cd") ||
+			value.Contains("pq") || value.Contains("xy");
 
-                func: (accu,current) => {
-                    if (accu.result)
-                        return accu;
-                    else if (accu.queue.Count() <= 2 )
-                        return (queue: accu.queue.Enqueue(current), 
-                            result: CheckSurrounding(accu.queue));
+		public static bool HasPair(this string value) => value
+			.Aggregate(
 
-                    return (queue: accu.queue.Enqueue(current).Dequeue(), 
-                        result: CheckSurrounding(accu.queue));
-                }, 
+				seed: (
+					hashset: ImmutableHashSet<string>.Empty,
+					result: false,
+					state: new Nullable<char>()),
 
-                resultSelector: accu => accu.result
-            );
+				func: (accu,current) => {
+					if (!accu.state.HasValue) {
+						return (hashset: accu.hashset, result: accu.result, state: current);
+					} else {
+						var tuple = accu.state.Value.ToString() + current;
+						if (accu.hashset.Contains(tuple))
+							return (hashset: accu.hashset, result: true, state: current);
+						return (hashset: accu.hashset.Add(tuple), result: accu.result, state: current);
+					}
+				},
 
-        private static bool CheckSurrounding(IEnumerable<char> queue) {
+				resultSelector: accu => accu.result
+			);
 
-            if (queue.Count() <=2)
-                return false;
-            var content = String.Concat(queue);
-            var hasSurounding = content.Last() == content.First();
-            return hasSurounding;
-                    
-        }
-    }
+		public static bool HasSurounding(this string value) => value
+			.Aggregate(
+				seed: (
+					queue: ImmutableQueue<char>.Empty,
+					result: false
+				),
+
+				func: (accu,current) => {
+					if (accu.result)
+						return accu;
+					var newQueue = accu.queue.Enqueue(current);
+					if (newQueue.Count()>3)
+						newQueue = newQueue.Dequeue();
+					return (queue: newQueue,
+						result: CheckSurrounding(newQueue));
+				},
+
+				resultSelector: accu => accu.result
+			);
+
+		private static bool CheckSurrounding(IEnumerable<char> queue) {
+
+			if (queue.Count() <=2)
+				return false;
+			var content = String.Concat(queue);
+			var hasSurounding = content.Last() == content.First();
+			return hasSurounding;
+		}
+	}
 }
