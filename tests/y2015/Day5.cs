9deb5ddcdaf1b95b22d744916cd79ff12377524e
Stefc <stefan.boether@gmail.com>
2019-05-06 17:53:59 +0200

    Day5 Part 2 funktioniert jetzt

--- tests/y2015/Day5.cs
+++ tests/y2015/Day5.cs
@@ -20,6 +20,31 @@ xxyxx is nice because it has a pair that appears twice and a letter that repeats
 uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
 ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
 
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
+def is_really_nice(s):
+    first = False
+    for i in range(len(s) - 3):
+        sub = s[i: i + 2]
+        if sub in s[i + 2:]:
+            first = True
+            print("{} is really nice and repeats with {}".format(s, sub))
+            break
+    if not first:
+        return False
+    second = False
+    for i in range(len(s) - 2):
+        if s[i] == s[i + 2]:
+            second = True
+            break
+    return second
+
+
 */
 
 	public class TestDay5
@@ -41,8 +66,13 @@ ieodomkazucvgmuy is naughty because it has a repeating letter with one between (
 		public void TestPair(string value) =>
 			Assert.True(StringClassifier.HasPair(value));
 
+		[Fact]
+		public void TestNoPair() =>
+			Assert.False(StringClassifier.HasPair("aaa"));
+
 		[Theory]
 		[InlineData("xyx")]
+		[InlineData("aaa")]
 		[InlineData("abcdefeghi")]
 		public void TestSurrounding(string value) =>
 			Assert.True(StringClassifier.HasSurounding(value));
@@ -62,13 +92,19 @@ ieodomkazucvgmuy is naughty because it has a repeating letter with one between (
 			var input = File
 				.ReadLines("tests/y2015/Day5.Input.txt");
 
+			Assert.Equal(113, input
+					.Where(StringClassifier.HasPair)
+					.Count());
+			Assert.Equal(412, input
+					.Where(StringClassifier.HasSurounding)
+					.Count());
+
+			Assert.Equal(55, input
+					.Where(StringClassifier.IsNicer)
+					.Count());
 			Assert.Equal(255, input
 					.Where(StringClassifier.IsNice)
 					.Count());
-			Assert.Equal(77, input
-					.Where(StringClassifier.IsNicer)
-					.Count());
-
 		}
 	}
 }
