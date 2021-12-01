6d4c5e5a9345d71c0525bcdcb6e809d814aefce7
stefc <Stefan.Boether@txs.de>
2021-12-01 07:34:40 +0100

    Umstellung auf Net6.0 & Day1 solved

---
+++ y2021/SonarSweep.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using System.Collections.Generic;
+using MoreLinq;
+
+
+namespace advent.of.code.y2021.day1
+{
+
+	// http://adventofcode.com/2021/day/1
+
+	static class SonarSweep
+	{
+		public static int CountIncreases(IEnumerable<int> values)
+		=> values.Zip( values.Skip(1), (a,b) => b-a ).Count( x => x > 0);
+
+		public static int CountIncreasesSliding(IEnumerable<int> values, int windowSize = 3)
+		=> CountIncreases(values.Window(windowSize).Select( window => window.Sum()));
+	}
+}
