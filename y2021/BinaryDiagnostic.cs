45fab4fa2aab5ae53009a3cd73b6d5210a7307f3
stefc <Stefan.Boether@txs.de>
2021-12-03 22:31:38 +0100

    BitArray extrahiert

--- y2021/BinaryDiagnostic.cs
+++ y2021/BinaryDiagnostic.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Diagnostics;
-using System.Text.RegularExpressions;
 using advent.of.code.common;
 
 namespace advent.of.code.y2021.day3;
@@ -14,7 +12,7 @@ class  BinaryDiagnostic
 
 		BitMatrix m = new BitMatrix(values);
 
-		var gamma = new BitArray(m.GetColumns().Select( MostCommonBit).ToArray());
+		var gamma = new BitArray(m.GetColumns().Select( bits => bits.MostCommonBit()).ToArray());
 
 		// var gamma = new BitArray(m.Transpose().Select(MostCommonBit).ToArray());
 		var epsilon = gamma.Negate();
@@ -39,7 +37,7 @@ class  BinaryDiagnostic
 		var index = 0;
 		while (m.Height != 1)
 		{
-			var mcb = MostCommonBit(m.GetColumn(index));
+			var mcb = m.GetColumn(index).MostCommonBit();
 			m = new BitMatrix(m.GetRows().Where(ba => ba.Get(index) == mcb));
 			index++;
 		}
@@ -51,44 +49,16 @@ class  BinaryDiagnostic
 		var index = 0;
 		while (m.Height != 1)
 		{
-			var lcb = LeastCommonBit(m.GetColumn(index));
+			var lcb = m.GetColumn(index).LeastCommonBit();
 			m = new BitMatrix(m.GetRows().Where(ba => ba.Get(index) == lcb));
 			index++;
 		}
 		return m.GetRows().Single().ToNumeral();
 	}
 
-	public static bool MostCommonBit(BitArray value)
-	{
-		var t = value.OfType<bool>().Count(b => b == true);
-		var f = value.OfType<bool>().Count(b => b == false);
-		return t == f ? true : t > f;
-	}
-
-	public static bool LeastCommonBit(BitArray value)
-	{
-		var t = value.OfType<bool>().Count(b => b == true);
-		var f = value.OfType<bool>().Count(b => b == false);
-		return t == f ? false : t < f;
-	}
+	
 }
 
-public static class BitArrayExtensions
-{
-	public static int ToNumeral(this BitArray binary)
-	{
-		if (binary == null)
-			throw new ArgumentNullException("binary");
-		if (binary.Length > 32)
-			throw new ArgumentException("must be at most 32 bits long");
-
-		var result = new int[1];
-		new BitArray(binary.Cast<bool>().Reverse().ToArray()).CopyTo(result, 0);
-		return result[0];
-	}
-
-	public static BitArray Negate(this BitArray bits) => new BitArray(bits).Not();
-}
 
 // [DebuggerDisplay("{DebuggerDisplay,nq}")]
 public class BitMatrix
@@ -111,8 +81,6 @@ public class BitMatrix
 
 	}
 
-	public BitArray ToBinary(int numeral) => ToBinary(this.w, numeral);
-
 	public static BitArray ToBinary(int w, int numeral)
 	{
 		var x = new BitArray(new[] { numeral });
@@ -125,8 +93,6 @@ public class BitMatrix
 
 	public int Weight => this.w;
 	public int Height => this.h;
-
-
 
 	public BitArray GetColumn(int col)
 	{
@@ -141,16 +107,5 @@ public class BitMatrix
 
 	public IEnumerable<BitArray> GetColumns() 
 	=> Enumerable.Range(0, this.Weight).Select( col => GetColumn(col)).ToArray();
-
-	public IEnumerable<BitArray> Transpose()
-	{
-		var output = Enumerable.Range(0, w).Select(_ => new BitArray(h)).ToArray();
-		for (int i = 0; i < w; i++)
-			for (int j = 0; j < h; j++)
-			{
-				output[i].Set(j, this.data.ElementAt(j)[i]);
-			}
-		return output;
-	}
 
 }
