fc13e57b354e8724737eacf3740f33b25a374bd5
stefc <Stefan.Boether@txs.de>
2021-12-03 22:49:58 +0100

    Day #4 vorbereitet

---
+++ y2021/Prepare.cs
@@ -0,0 +1,14 @@
+namespace advent.of.code.y2021.day4;
+
+// http://adventofcode.com/2021/day/4
+
+class Prepare
+{
+	public int A(IEnumerable<string> values)
+	=> 42;
+
+	public int B(IEnumerable<string> values)
+	=> 43;
+
+}
+
